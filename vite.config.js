61f5ef7930d7aa3818b7008801fff6079762351a
hardivile <hardivile@gmail.com>
2025-10-16 06:29:23 +0200

    Prepare deploy to GitHub Pages: base config and deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+   
+
+//   ],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/disney/', // 👈 très important pour GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
-   
-
   ],
 })
